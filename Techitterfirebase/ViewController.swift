eb5d9588ce34a67cd25cc9a4fd9f89904c9c7058
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-09-21 18:24:00 +0900

    Initial Commit

---
+++ Techitterfirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Techitterfirebase
+//
+//  Created by 森田貴帆 on 2020/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
